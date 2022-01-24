dadfb1af183cee9df1d655c4102a12bc5d28e437
hilquiasfmelo <hilquiasfmelo@hotmail.com>
2022-01-24 20:38:04 -0300

    Fundamentos do Next.js

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { Title } from '../styles/pages/Home'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Title>Hello World</Title>
+    </div>
+  )
+}
+
+export default Home;
